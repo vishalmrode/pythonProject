8fa1ed2876dea6ebc51ac9870c2fb35b5626b069
vishalmrode <vishalmrode@outlook.com>
2024-08-28 22:26:07 +0530

    Initial commit for python project

---
+++ CSSlocators.py
@@ -0,0 +1,6 @@
+from playwright.sync_api import  sync_playwright
+
+with sync_playwright() as play:
+    browser = play.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto('https://demo.automationtesting.in/Register.html')
