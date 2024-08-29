838b922db972a2d42bee89b2af616223885eb2af
vishalmrode <vishalmrode@outlook.com>
2024-08-29 14:42:23 +0530

    CSS Locators

--- CSSlocators.py
+++ CSSlocators.py
@@ -3,4 +3,17 @@ from playwright.sync_api import  sync_playwright
 with sync_playwright() as play:
     browser = play.chromium.launch(headless=False)
     page = browser.new_page()
-    page.goto('https://demo.automationtesting.in/Register.html')
+    page.goto('https://demo.automationtesting.in/')
+
+    # CSS Selector usage
+    # Id - ---  # (pound)
+    # Class - ---.(dot)
+    # attribute - -- tagname[attribute = "value"]
+    page.locator('#email').click()
+    page.locator('#email').fill('abc@abc.com')
+    page.wait_for_timeout(3000)
+    page.click('#enterimg')
+    print('email-id entered and clicked on button')
+    print(page.title())
+    page.wait_for_timeout(3000)
+
